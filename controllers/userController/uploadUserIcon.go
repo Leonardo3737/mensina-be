e4b9afd42d113463ecd9e9b9cb92558efa3aac89
Clara Helena <106986725+clarasenx@users.noreply.github.com>
2024-11-24 15:25:38 -0400

    feat: implement cloudinary api (incomplet)

--- controllers/userController/uploadUserIcon.go
+++ controllers/userController/uploadUserIcon.go
@@ -1,12 +1,12 @@
 package userController
 
 import (
+	"io"
 	"mensina-be/config"
 	"mensina-be/core/useCases/userUseCase"
 	"mensina-be/utils"
 
 	"github.com/gin-gonic/gin"
-	"github.com/olahol/go-imageupload"
 )
 
 // @Summary Upload de ícone de usuário
@@ -25,20 +25,29 @@ func UploadUserIcon(c *gin.Context) {
 		return
 	}
 
-	userIcon, err := imageupload.Process(c.Request, "file")
-
+	// Obter o arquivo do formulário
+	file, _, err := c.Request.FormFile("file")
 	if err != nil {
-		restErr := config.NewInternaErr(err.Error())
+		restErr := config.NewBadRequestErr("Erro ao processar o arquivo enviado")
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	defer file.Close()
+
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		restErr := config.NewInternaErr("Erro ao processar o arquivo enviado")
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 
-	userCaseErr := userUseCase.UploadUserIcon(userId, userIcon)
-
+	// Chamar o use case para processar o ícone do usuário
+	userCaseErr := userUseCase.UploadUserIcon(userId, fileBytes)
 	if userCaseErr != nil {
 		c.JSON(userCaseErr.Code, userCaseErr)
 		return
 	}
 
+	// Retornar sucesso
 	c.Status(201)
 }
--- core/useCases/userUseCase/uploadUserIcon.go
+++ core/useCases/userUseCase/uploadUserIcon.go
@@ -1,30 +1,93 @@
 package userUseCase
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"mensina-be/config"
+	"mime/multipart"
+	"net/http"
 	"os"
-	"path/filepath"
-
-	"github.com/olahol/go-imageupload"
 )
 
-func UploadUserIcon(userId uint, icon *imageupload.Image) *config.RestErr {
-	fileName := fmt.Sprintf("usericon_%d.png", userId)
-	uploadDir := filepath.Join("uploads", "user_icons")
-	filePath := filepath.Join(uploadDir, fileName)
+type APIResponse struct {
+	AssetID      string    `json:"asset_id"`
+	PublicID     string    `json:"public_id"`
+	Version      interface{}     `json:"version"`
+	VersionID    string    `json:"version_id"`
+	Signature    string    `json:"signature"`
+	Width        interface{}       `json:"width"`
+	Height       interface{}       `json:"height"`
+	Format       string    `json:"format"`
+	ResourceType string    `json:"resource_type"`
+	CreatedAt    string    `json:"created_at"`
+	Tags         []interface{}  `json:"tags"`
+	Bytes        interface{}       `json:"bytes"`
+	Type         string    `json:"type"`
+	ETag         string    `json:"etag"`
+	Placeholder  bool      `json:"placeholder"`
+	URL          string    `json:"url"`
+	SecureURL    string    `json:"secure_url"`
+	AssetFolder  string    `json:"asset_folder"`
+	DisplayName  string    `json:"display_name"`
+}
 
-	err := os.MkdirAll(uploadDir, os.ModePerm)
+func UploadUserIcon(userId uint, icon []byte) *config.RestErr {
+
+	url := os.Getenv("URL_UPLOAD_IMAGE")
+	preset := os.Getenv("UPLOAD_PRESET")
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	writer.WriteField("upload_preset", preset)
+
+	// Criar o campo para a imagem
+	part, err := writer.CreateFormFile("file", fmt.Sprintf("usericon_%d.png", userId))
 	if err != nil {
-		return config.NewInternaErr("Erro ao criar diretório de upload")
+		return config.NewInternaErr("Erro ao criar campo icon no FormData")
 	}
 
-	// Salvar a imagem como PNG no sistema de arquivos
-	err = icon.Save(filePath)
+	// Escrever a imagem no campo
+	_, err = io.Copy(part, bytes.NewReader(icon))
 	if err != nil {
-		fmt.Printf("%+v\n", err)
-		return config.NewInternaErr("Erro ao salvar a imagem")
+		return config.NewInternaErr("Erro ao adicionar imagem ao FormData")
 	}
+
+	// Finalizar o writer para concluir o FormData
+	err = writer.Close()
+	if err != nil {
+		return config.NewInternaErr("Erro ao finalizar o FormData")
+	}
+
+	req, _ := http.NewRequest("POST", url, body)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+			return config.NewInternaErr("Erro ao enviar requisição para o servidor")
+	}
+	defer resp.Body.Close()
+
+	resBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+			return config.NewInternaErr("Erro ao processar resposta da API")
+	}
+
+	if resp.StatusCode != http.StatusOK {
+			fmt.Printf("Erro da API: %s\n", string(resBody))
+			return config.NewInternaErr("Erro ao enviar imagem: " + string(resBody))
+	}
+
+	// Decodificar o JSON da resposta
+	var response APIResponse
+	err = json.Unmarshal(resBody, &response)
+	if err != nil {
+			return config.NewInternaErr("Erro ao decodificar JSON: " + err.Error())
+	}
+
+	fmt.Println(response.SecureURL)
+
 
 	return nil
 }
